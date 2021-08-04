d58441c1bc08570125a138c4d986ab4561d4f8de
Kenneth Ceglia <KPZNet@outlook.com>
2021-08-04 13:57:58 -0700

    alien vc

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ import numpy as np
 
 class Sorter:
 
+    def __init__(self, plot_sort = False):  
+        self.PLOT_SORT = plot_sort
+
 
     def bubble_sort(self, array) :
 
-        y = [str ( i ) for i in range ( len ( array ) )]
-        plt.ion()
+        if self.PLOT_SORT: 
+            y = [str ( i ) for i in range ( len ( array ) )]      
+            plt.ion()
 
+        
         n = len ( array )
         for i in range ( n ) :
             already_sorted = True
@@ -19,8 +24,9 @@ class Sorter:
                 if array[j] > array[j + 1] :
                     array[j], array[j + 1] = array[j + 1], array[j]
 
-                    self.update_plot(plt, array, y)
-                    
+                    if self.PLOT_SORT:
+                        self.update_plot(plt, array, y)
+                
                     already_sorted = False
             if already_sorted :
                 break
@@ -50,7 +56,6 @@ class Sorter:
         return result
 
     def merge_sort(self, array) :
-
         if len ( array ) < 2 :
             return array
         midpoint = len ( array ) // 2
@@ -59,9 +64,10 @@ class Sorter:
             right= self.merge_sort ( array[midpoint :] ) )
 
     def insertion_sort(self, array):
-        
-        y = [str ( i ) for i in range ( len ( array ) )]
-        plt.ion()
+            
+        if self.PLOT_SORT:
+            y = [str ( i ) for i in range ( len ( array ) )]
+            plt.ion()
         
         for i in range(1, len(array)):
             key_item = array[i]
@@ -69,11 +75,47 @@ class Sorter:
             while j >= 0 and array[j] > key_item:
                 array[j + 1] = array[j]
                 j -= 1
-
-                self.update_plot(plt, array, y)
+                if self.PLOT_SORT:
+                    self.update_plot(plt, array, y)
 
             array[j + 1] = key_item
         return array
+
+
+    def partition(self, arr, low, high):
+        i = (low-1)		 # index of smaller element
+        pivot = arr[high]	 # pivot
+
+        for j in range(low, high):
+
+            # If current element is smaller than or
+            # equal to pivot
+            if arr[j] <= pivot:
+
+                # increment index of smaller element
+                i = i+1
+                arr[i], arr[j] = arr[j], arr[i]
+
+        arr[i+1], arr[high] = arr[high], arr[i+1]
+        return (i+1)
+
+    def quickSort(self, arr, low, high):
+        if len(arr) == 1:
+            return arr
+        if low < high:
+
+            # pi is partitioning index, arr[p] is now
+            # at right place
+            pi = self.partition(arr, low, high)
+
+            # Separately sort elements before
+            # partition and after partition
+            self.quickSort(arr, low, pi-1)
+            self.quickSort(arr, pi+1, high)
+
+    def quick_sort(self, array):
+        n = len(array)
+        self.quickSort(array, 0, n-1)
 
     def update_plot(self, plt, array, y):
         plt.clf()
@@ -85,22 +127,29 @@ print("Sort Comparisons")
 s = Sorter()
 
 
+ARRAY_SIZE = 8000
 
-rand_array = [random.randint(0,100) for i in range(20)]
+rand_array = [random.randint(0,100) for i in range(ARRAY_SIZE)]
 start_time_bs = time.perf_counter()
 sorted_rand_array_bs = s.bubble_sort(rand_array)
 stop_time_bs = time.perf_counter()
 
-rand_array = [random.randint(0,100) for i in range(20)]
+rand_array = [random.randint(0,100) for i in range(ARRAY_SIZE)]
 start_time_ms = time.perf_counter()
-#sorted_rand_array_ms = s.merge_sort(rand_array)
+sorted_rand_array_ms = s.merge_sort(rand_array)
 stop_time_ms = time.perf_counter()
 
-rand_array = [random.randint(0,100) for i in range(20)]
+rand_array = [random.randint(0,100) for i in range(ARRAY_SIZE)]
+start_time_qs = time.perf_counter()
+sorted_rand_array_qs = s.quick_sort(rand_array)
+stop_time_qs = time.perf_counter()
+
+rand_array = [random.randint(0,100) for i in range(ARRAY_SIZE)]
 start_time_is = time.perf_counter()
 sorted_rand_array_is = s.insertion_sort(rand_array)
 stop_time_is = time.perf_counter()
 
-#print(f"Bubble Sort: {stop_time_bs - start_time_bs:0.4f} seconds")
-#print(f"Merge Sort: {stop_time_ms - start_time_ms:0.4f} seconds")
-#print(f"Insertion Sort: {stop_time_is - start_time_is:0.4f} seconds")
+print(f"Bubble Sort: {stop_time_bs - start_time_bs:0.4f} seconds")
+print(f"Merge Sort: {stop_time_ms - start_time_ms:0.4f} seconds")
+print(f"Insertion Sort: {stop_time_is - start_time_is:0.4f} seconds")
+print(f"Quick Sort: {stop_time_qs - start_time_qs:0.4f} seconds")
