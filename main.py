354eccadc3a8c6f922066cbe7295c87f1b6837f6
KPZNET <kpznet@outlook.com>
2021-08-03 17:57:15 -0700

    mac dev

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import random
 import time
 import matplotlib.pyplot as plt
+import numpy as np
 
 class Sorter:
 
-    def plot_bar_graph(self, x):
-        sx = len ( x )
-        y =  [str(i) for i in range(sx)]
-        sy = len(y)
-        plt.bar (x,y)
-        plt.title ( 'Sort' )
-        plt.xlabel ( 'X' )
-        plt.ylabel ( 'Y' )
-        plt.show ()
-
 
     def bubble_sort(self, array) :
+
+        y = [str ( i ) for i in range ( len ( array ) )]
+        plt.ion()
+
         n = len ( array )
         for i in range ( n ) :
             already_sorted = True
-            self.plot_bar_graph(array)
+
             for j in range ( n - i - 1 ) :
                 if array[j] > array[j + 1] :
                     array[j], array[j + 1] = array[j + 1], array[j]
+
+                    plt.clf()
+                    plt.bar ( y, array )
+                    plt.draw()
+                    plt.pause(0.00001)
+                    
                     already_sorted = False
             if already_sorted :
                 break
@@ -51,6 +52,7 @@ class Sorter:
         return result
 
     def merge_sort(self, array) :
+
         if len ( array ) < 2 :
             return array
         midpoint = len ( array ) // 2
@@ -59,22 +61,36 @@ class Sorter:
             right= self.merge_sort ( array[midpoint :] ) )
 
     def insertion_sort(self, array):
+        
+        y = [str ( i ) for i in range ( len ( array ) )]
+        plt.ion()
+        
         for i in range(1, len(array)):
             key_item = array[i]
             j = i - 1
             while j >= 0 and array[j] > key_item:
                 array[j + 1] = array[j]
                 j -= 1
+
+                plt.clf()
+                plt.bar ( y, array )
+                plt.draw()
+                plt.pause(0.00001)
+
             array[j + 1] = key_item
         return array
 
 print("Sort Comparisons")
 s = Sorter()
-rand_array = [random.randint(0,100) for i in range(100)]
+
+
+
+rand_array = [random.randint(0,100) for i in range(20)]
+
 
 
 start_time_bs = time.perf_counter()
-sorted_rand_array_bs = s.bubble_sort(rand_array)
+#sorted_rand_array_bs = s.bubble_sort(rand_array)
 stop_time_bs = time.perf_counter()
 
 start_time_ms = time.perf_counter()
@@ -82,9 +98,9 @@ sorted_rand_array_ms = s.merge_sort(rand_array)
 stop_time_ms = time.perf_counter()
 
 start_time_is = time.perf_counter()
-sorted_rand_array_is = s.insertion_sort(rand_array)
+#sorted_rand_array_is = s.insertion_sort(rand_array)
 stop_time_is = time.perf_counter()
 
 #print(f"Bubble Sort: {stop_time_bs - start_time_bs:0.4f} seconds")
-print(f"Merge Sort: {stop_time_ms - start_time_ms:0.4f} seconds")
-print(f"Insertion Sort: {stop_time_is - start_time_is:0.4f} seconds")
+#print(f"Merge Sort: {stop_time_ms - start_time_ms:0.4f} seconds")
+#print(f"Insertion Sort: {stop_time_is - start_time_is:0.4f} seconds")
