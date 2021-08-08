569e34db57885bcc96ddeb3f0ef1fe09b59952df
Kenneth Ceglia <KPZNet@outlook.com>
2021-08-08 13:49:13 -0700

    devs

--- bubble.py
+++ bubble.py
@@ -9,8 +9,7 @@ class BubbleSort:
             already_sorted = True
             for j in range ( n - i - 1 ) :
                 if array[j] > array[j + 1] :
-                    array[j], array[j + 1] = array[j + 1], array[j]
-                
+                    array[j], array[j + 1] = array[j + 1], array[j]   
                     already_sorted = False
             if already_sorted :
                 break
